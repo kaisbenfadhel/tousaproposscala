2eb67329120f8bc1827855e2c64d75d178b8fbe8
kaisbenfadhel <ben.fadhel.kais@gmail.com>
2019-07-26 18:23:45 +0200

    commit chapitre 3 :Penser en termes de fonctions

--- src/main/scala/chapitre/trois/Penser/en/termes/de/fonctions/tutoriel_04/FunctionWithOptionReturnType.scala
+++ src/main/scala/chapitre/trois/Penser/en/termes/de/fonctions/tutoriel_04/FunctionWithOptionReturnType.scala
@@ -20,5 +20,30 @@ object FunctionWithOptionReturnType extends App {
   val todayCoupon: Option[String] = dailyCouponCode()
   println(s"Code de coupon du jour = ${todayCoupon.getOrElse("Pas de coupon Aujourd'hui")}")
 
+  println(s"\nEtape 3: Comment appeler une fonction avec le type de retour Option en utilisant pattern matching")
+  dailyCouponCode() match {
+    case Some(couponCode) => println(s"Code de coupon du jour = $couponCode")
+    case None => println(s"Désolé, il n'y a pas de code promo aujourd'hui!")
 
+      println(s"\nEtape 4: Comment appeler une fonction avec le type de retour Option à l’aide de la fonction map ()")
+      dailyCouponCode().map(couponCode => println(s"Code de coupon du jour = $couponCode"))
+  }
+
+
+  println("\nStep 5: Passez en revue la fonction CalculateDonutCost () du tutoriel précédent")
+  def calculateDonutCost(donutName: String, quantity: Int, couponCode: Option[String]): Double = {
+    println(s"Calcul du coût pour $donutName, quantity = $quantity")
+
+    couponCode match {
+      case Some(coupon) =>
+        val discount = 0.1 // Let's simulate a 10% discount
+      val totalCost = 2.50 * quantity * (1 - discount)
+        totalCost
+
+      case None => 2.50 * quantity
+    }
+  }
+
+  // appliquer le code de coupon quotidien si nous en avons un
+  println(s"""Coût total avec code de coupon quotidien = ${calculateDonutCost("Donut Glacée", 5, dailyCouponCode())}""")
 }
